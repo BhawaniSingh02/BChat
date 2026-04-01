3fe7610576bf0dc62c6da38f7a9e07c2a40c3484
Bhawani Singh <bhawanisinghinda02@gmail.com>
2026-04-01 10:19:48 +0530

    fix: a bug

--- chat-app-backend/src/main/java/com/substring/chat/controllers/HealthController.java
+++ chat-app-backend/src/main/java/com/substring/chat/controllers/HealthController.java
@@ -1,4 +1,4 @@
-package com.substring.chat.controllers;r;  // apna package name daalo
+package com.substring.chat.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
